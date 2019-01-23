855d5ced5a36a31a872de69ac05d178dfc389998
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-01-23 22:55:07 +0100

    Added all the user interface

--- app/src/main/java/com/example/a6god_tha_godfather_/parkingsharingapp/MainActivity.java
+++ app/src/main/java/com/example/a6god_tha_godfather_/parkingsharingapp/MainActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button goToSignUp ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     public void signUpAlt(View view){
--- app/src/main/java/com/example/a6god_tha_godfather_/parkingsharingapp/OwnerActivity.java
+++ app/src/main/java/com/example/a6god_tha_godfather_/parkingsharingapp/OwnerActivity.java
@@ -1,7 +1,9 @@
 package com.example.a6god_tha_godfather_.parkingsharingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 class OwnerActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ class OwnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
